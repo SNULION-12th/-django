e94bd200d173df46cd7565bfb3c8e9007ec615d0
minhouu <tortor1403@gmail.com>
2024-02-01 19:15:21 +0900

    feat: 7th week seminar contents & hw solution

--- post/views.py
+++ post/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# FBV는 토글 내부 내용에서 확인 가능
+# @api_view(['POST'])
+# def CreatePostView(request):
+#     title = request.data.get('title')
+#     content = request.data.get('content')
+#     post = Post.objects.create(title=title, content=content)
+#     return Response({"msg":f"'{post.title}'이 생성되었어요!"})
+
+# @api_view(['GET'])
+# def ReadAllPostView(request):
+#     posts = Post.objects.all()
+#     contents = [{post.title:post.content} for post in posts]
+#     return Response({"posts":contents})
+
+class PostListView(APIView):
+    def get(self, request): 
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+
+    def post(self, request):
+        title = request.data.get('title')
+        content = request.data.get('content')
+        if not title or not content:
+            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        post = Post.objects.create(title=title, content=content)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class PostDetailView(APIView):
+    def get(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def delete(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def put(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        title = request.data.get('title')
+        content = request.data.get('content')
+        if not title or not content:
+            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
+        post.title = title
+        post.content = content
+        post.save()
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
