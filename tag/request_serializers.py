f8ca925288e89a02a2b43bbbafa9c60e9a6b1854
Jack-Chagarr <netamijr@gmail.com>
2024-04-07 02:58:04 +0900

    fix: remove unnecessary field in request serializers

--- tag/request_serializers.py
+++ tag/request_serializers.py
@@ -4,5 +4,4 @@ from account.request_serializers import SignInRequestSerializer
 
 
 class TagListRequestSerialiizer(serializers.Serializer):
-    author = SignInRequestSerializer()
     content = serializers.CharField()
