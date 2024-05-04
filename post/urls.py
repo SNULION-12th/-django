4458ffebb1aaf27540edaa22d2774e56795d2af2
Rokhee LEE <rockey020312@snu.ac.kr>
2024-05-04 17:13:14 +0900

    seminar

---
+++ post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import PostListView, PostDetailView
+
+app_name = 'post'
+urlpatterns = [
+    path("", PostListView.as_view()), ### 추가
+    path("<int:post_id>/", PostDetailView.as_view()), ### 추가
+
+]
