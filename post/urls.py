12e73676e58680eeaa3299112b4c2504b8d04a62
Rokhee LEE <rockey020312@snu.ac.kr>
2024-05-06 20:01:13 +0900

    feat: hw done

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,6 @@ from .views import PostListView, PostDetailView
 app_name = 'post'
 urlpatterns = [
     path("", PostListView.as_view()), ### 추가
-    path("<int:post_id>/", PostDetailView.as_view()), ### 추가
-
+    path("<int:post_id>", PostDetailView.as_view()), ### 추가
+    path("<int:post_id>/", PostDetailView.as_view()),
 ]
