12f8b2ee224e683990d0f997e0dbf9d8b276cc20
minseo <minsu7476@naver.com>
2024-05-07 20:58:20 +0900

    feat: i love eunjae

--- post/views.py
+++ post/views.py
@@ -69,3 +69,15 @@ class PostDetailView(APIView):
         post.delete()
         # 삭제를 성공한 것이라 'No content'
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def put(self, request, post_id):
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        
+        post.title = request.data.get("title")
+        post.content = request.data.get("content")
+        post.save()
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
