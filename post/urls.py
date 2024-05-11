f8a4c1f79cb348587e4b898fdb45e3717333cce0
Rokhee LEE <rockey020312@snu.ac.kr>
2024-05-11 16:28:52 +0900

    week8 seminar fin

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, LikeView
 
 app_name = 'post'
 urlpatterns = [
-    path("", PostListView.as_view()), ### 추가
-    path("<int:post_id>", PostDetailView.as_view()), ### 추가
+    path("", PostListView.as_view()),
     path("<int:post_id>/", PostDetailView.as_view()),
+    path("<int:post_id>/like/", LikeView.as_view()),
 ]
