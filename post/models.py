3344538f10c2ecefa9ba23be09dc06060b4ed0bc
hoyah <hoyaho03@snu.ac.kr>
2024-05-11 16:30:32 +0900

    feat : initial setting

---
+++ post/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=256)
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
---
+++ post/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from .views import  PostListView, PostDetailView
+
+app_name = 'post'
+urlpatterns = [
+    # CBV url path
+    path("", PostListView.as_view()), ### 추가
+    path("<int:post_id>/", PostDetailView.as_view()), ### 추가
+
+]
