28c03892688b1eb1861d125ed2b906aff0b0df24
sono <rhj080471@gmail.com>
2024-05-13 20:01:24 +0900

    세미나 까지

--- post/views.py
+++ post/views.py
@@ -1,117 +1,258 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .request_serializers import PostListRequestSerializer,PostDetailRequestSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from .models import Post
+from .models import Post,Like
 from .serializers import PostSerializer
 from drf_yasg.utils import swagger_auto_schema
 from django.shortcuts import get_object_or_404
+from account.models import User
+from tag.models import Tag
+from account.request_serializers import SignInRequestSerializer
+
 
 class PostListView(APIView):
-	### 얘네가 class inner function 들! ###
     @swagger_auto_schema(
-            operation_id='게시글 목록 조회',
-            operation_description='게시글 목록을 조회합니다.',
-            responses={200: PostSerializer(many=True)}
-        )
-    def get(self, request): 
-        # Post의 오브젝트를 모두 가져오기
+        operation_id="게시글 목록 조회",
+        operation_description="게시글 목록을 조회합니다.",
+        responses={
+            200: PostSerializer(many=True),
+            404: "Not Found",
+            400: "Bad Request",
+        },
+    )
+    def get(self, request):
         posts = Post.objects.all()
-        # # 가져온 것들을 리스트로 만들어줌
-        # contents = [{"id":post.id,
-        #              "title":post.title,
-        #              "content":post.content,
-        #              "created_at":post.created_at
-        #              } for post in posts]
-        # # 데이터와 상태코드를 응답
-        # return Response(contents, status=status.HTTP_200_OK)
-        # serializer 사용
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
 
     @swagger_auto_schema(
-            operation_id='게시글 생성',
-            operation_description='게시글을 생성합니다.',
-            request_body=PostSerializer,
-            responses={201: PostSerializer}
-        )
+        operation_id="게시글 생성",
+        operation_description="게시글을 생성합니다.",
+        request_body=PostListRequestSerializer,
+        responses={201: PostSerializer, 404: "Not Found", 400: "Bad Request"},
+    )
     def post(self, request):
-        # 요청 바디를 통해 들어온 데이터를 가져옴
-        title = request.data.get('title')
-        content = request.data.get('content')
-        # 예외처리 title이나 content가 없을 때 
+        title = request.data.get("title")
+        content = request.data.get("content")
+        tag_contents = request.data.get("tags")
+        author_info = request.data.get("author")
+        if not author_info:
+            return Response(
+                {"detail": "author field missing."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        username = author_info.get("username")
+        password = author_info.get("password")
+        if not username or not password:
+            return Response(
+                {"detail": "[username, password] fields missing in author"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         if not title or not content:
-            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
-        # 새로운 post 객체를 만들기
-        post = Post.objects.create(title=title, content=content)
-        # # 새롭게 만든 post 객체를 응답해준다.
-        # return Response({
-        #     "id":post.id,
-        #     "title":post.title,
-        #     "content":post.content,
-        #     "created_at":post.created_at
-        #     }, status=status.HTTP_201_CREATED)
+            return Response(
+                {"detail": "[title, content] fields missing."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            author = User.objects.get(username=username)
+            if not author.check_password(password):
+                return Response(
+                    {"detail": "Password is incorrect."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            post = Post.objects.create(title=title, content=content, author=author)
+        except:
+            return Response(
+                {"detail": "User Not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        if tag_contents is not None:
+            for tag_content in tag_contents:
+                if not Tag.objects.filter(content=tag_content).exists():
+                    post.tags.create(content=tag_content)
+                else:
+                    post.tags.add(Tag.objects.get(content=tag_content))
+
         serializer = PostSerializer(post)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 class PostDetailView(APIView):
     @swagger_auto_schema(
-            operation_id='게시글 상세 조회',
-            operation_description='게시글 1개의 상세 정보를 조회합니다.',
-            responses={200: PostSerializer}
-        )
+        operation_id="게시글 상세 조회",
+        operation_description="게시글 1개의 상세 정보를 조회합니다.",
+        responses={200: PostSerializer, 400: "Bad Request"},
+    )
     def get(self, request, post_id):
         try:
             post = Post.objects.get(id=post_id)
         except:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
-        # return Response({
-        #     "id":post.id,
-        #     "title":post.title,
-        #     "content":post.content,
-        #     "created_at":post.created_at
-        #     }, status=status.HTTP_200_OK)
-        serializer = PostSerializer(post)
+
+        serializer = PostSerializer(instance=post)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     @swagger_auto_schema(
-            operation_id='게시글 삭제',
-            operation_description='게시글을 삭제합니다.',
-            responses={204: 'No Content', 404: 'Not Found'}
-        )
+        operation_id="게시글 삭제",
+        operation_description="게시글을 삭제합니다.",
+        request_body=SignInRequestSerializer,
+        responses={204: "No Content", 404: "Not Found", 400: "Bad Request"},
+    )
     def delete(self, request, post_id):
         try:
             post = Post.objects.get(id=post_id)
         except:
-            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"detail": "Post Not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        author_info = request.data
+        if not author_info:
+            return Response(
+                {"detail": "author field missing."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        username = author_info.get("username")
+        password = author_info.get("password")
+        if not username or not password:
+            return Response(
+                {"detail": "[username, password] fields missing."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        try:
+            author = User.objects.get(username=username)
+            if not author.check_password(password):
+                return Response(
+                    {"detail": "Password is incorrect."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            if post.author != author:
+                return Response(
+                    {"detail": "You are not the author of this post."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+        except:
+            return Response(
+                {"detail": "User Not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
     @swagger_auto_schema(
-            operation_id='게시글 수정',
-            operation_description='게시글을 수정합니다.',
-            request_body=PostSerializer,
-            responses={200: PostSerializer}
-        )
+        operation_id="게시글 수정",
+        operation_description="게시글을 수정합니다.",
+        request_body=PostDetailRequestSerializer,
+        responses={200: PostSerializer, 404: "Not Found", 400: "Bad Request"},
+    )
     def put(self, request, post_id):
-        
-        # 요청 바디를 통해 들어온 데이터를 가져옴
-        title = request.data.get('title')
-        content = request.data.get('content')
-        
-        # 요청된 post_id를 가진 포스트를 가져옴
-        post = get_object_or_404(Post, pk=post_id)
-        
-        # title이나 content가 없을 때 예외처리
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response(
+                {"detail": "Post not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        author_info = request.data.get("author")
+        if not author_info:
+            return Response(
+                {"detail": "author field missing."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        username = author_info.get("username")
+        password = author_info.get("password")
+        try:
+            author = User.objects.get(username=username)
+            if not author.check_password(password):
+                return Response(
+                    {"detail": "Password is incorrect."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            if post.author != author:
+                return Response(
+                    {"detail": "You are not the author of this post."},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
+        except:
+            return Response(
+                {"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        title = request.data.get("title")
+        content = request.data.get("content")
         if not title or not content:
-            return Response({"detail": "[title, content] fields missing."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # 가져온 포스트를 수정함
+            return Response(
+                {"detail": "[title, content] fields missing."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         post.title = title
         post.content = content
+
+        tag_contents = request.data.get("tags")
+        if tag_contents is not None:
+            post.tags.clear()
+            for tag_content in tag_contents:
+                if not Tag.objects.filter(content=tag_content).exists():
+                    post.tags.create(content=tag_content)
+                else:
+                    post.tags.add(Tag.objects.get(content=tag_content))
         post.save()
-        
-        serializer = PostSerializer(post)
+        serializer = PostSerializer(instance=post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class LikeView(APIView):
+    @swagger_auto_schema(
+        operation_id="좋아요 토글",
+        operation_description="좋아요를 토글합니다. 이미 좋아요가 눌려있으면 취소합니다.",
+        request_body=SignInRequestSerializer,
+        responses={200: PostSerializer, 404: "Not Found", 400: "Bad Request"},
+    )
+    def post(self, request, post_id):
+
+        ### 1 ###
+        try:
+            post = Post.objects.get(id=post_id)
+        except:
+            return Response(
+                {"detail": "Post not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+        author_info = request.data
+        if not author_info:
+            return Response(
+                {"detail": "author field missing."}, status=status.HTTP_400_BAD_REQUEST
+            )
+        username = author_info.get("username")
+        password = author_info.get("password")
+        if not username or not password:
+            return Response(
+                {"detail": "[username, password] fields missing."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        ### 2 ###
+        try:
+            author = User.objects.get(username=username)
+            if not author.check_password(password):
+                return Response(
+                    {"detail": "Password is incorrect."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except:
+            return Response(
+                {"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        ### 3 ###
+        is_liked = post.like_set.filter(user=author).count() > 0
+
+        ### 4 ###
+        if is_liked == True:
+            post.like_set.get(user=author).delete()
+            print("좋아요 취소")
+        else:
+            Like.objects.create(user=author, post=post)
+            print("좋아요 누름")
+
+        serializer = PostSerializer(instance=post)
         return Response(serializer.data, status=status.HTTP_200_OK)
