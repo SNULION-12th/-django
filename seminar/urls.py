fb31d7dd2c27b4955d35766bfb17ee5121efb773
almighty5 <almighty5@snu.ac.kr>
2024-05-14 20:24:29 +0900

    week8-subin

--- seminar/urls.py
+++ seminar/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# basic django imports
 from django.contrib import admin
 from django.urls import path, include
+
+# swagger imports
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# swagger settings
 schema_view = get_schema_view(
     openapi.Info(
         title="LIKELION Blog API",
@@ -34,5 +38,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/post/', include('post.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
+    path('api/account/', include('account.urls')),
+    path('api/tag/', include('tag.urls')),
+    # swagger path
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
