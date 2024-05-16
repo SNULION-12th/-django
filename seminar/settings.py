311e9587b062866eb20304d21bd5ac61d319bbaa
unknownburphy <chrisandjj1218@gmail.com>
2024-05-17 03:26:48 +0900

    fix: fix CORS Error completely

--- seminar/settings.py
+++ seminar/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
     "account",
     "tag",
     "comment",
+    "corsheaders",
     "rest_framework",
+    "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
 ]
 
